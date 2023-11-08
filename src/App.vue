cb8af94977c302e6159f83717a1eb6ca87ad8759
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-08 17:01:17 +0100

    title-done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-
+    <h1>{{ AppTitle }}</h1>
   </div>
 </template>
 
 <script>
   export default {
-    
+    data(){
+      return {
+        AppTitle: 'La mia prima app con Vite!'
+      }
+    }
   }
 </script>
 
